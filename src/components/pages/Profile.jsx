b42258a70491a53e6d7e32c634661ccd80a9f516
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-03-30 23:21:12 +0800

    orders back end

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,45 +1,48 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
-import MapTest from "../m-bottom/MapTest";
+import { useContext, useState } from "react";
+//import MapTest from "../m-bottom/MapTest";
 import { UserContext } from "../mainComponent/UserProvider";
 import "../../Style/profile.css";
 
 export default function Profile(props) {
   const { user } = useContext(UserContext);
   const [orders, setOrders] = useState([]);
+  const [stock, setStock] = useState([]);
   const users = user && user.data;
-  const products = props.products;
+  // const products = props.products;
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:2000/order")
-      .then((orders) => setOrders(orders.data));
-  }, []);
+ function profileOrder(){
+  axios.post("http://localhost:2000/profileOrder", {userId:users._id}).then((res)=>(setOrders(res.data.product),setStock(res.data.stock)))
+  
 
-  let findOrder = orders && orders.find((order) => order.userId === users._id);
+ }
+  
+console.log("zahialguud",stock)
+  // let findOrder = orders && orders.find((order) => order.userId === users._id);
 
-  let findProduct =
-    products &&
-    products.filter((orderPro) => {
-      return findOrder?.orderProducts.find((product) => {
-        return product.id === orderPro.id;
-      });
-    });
+  // let findProduct =
+  //   products &&
+  //   products.filter((orderPro) => {
+  //     return findOrder?.orderProducts.find((product) => {
+  //       return product.id === orderPro.id;
+  //     });
+  //   });
 
-  console.log("find order", findOrder);
-  console.log("find product", findProduct);
+  // console.log("find order", findOrder);
+  // console.log("find product", findProduct);
   return (
     <>
       <div className="profileContainer">
         <section>
           <h1>Hello {users && users.userName}</h1>
+          <button onClick={profileOrder}>zahialguudaa harah</button>
         </section>
-        <section>
+        {/* <section>
           <h1>tanii zahialsan baraanuud </h1>
           {findProduct.map((order, index) => {
             return <div key={index}>{<h4>{order.name}</h4>}</div>;
           })}
-        </section>
+        </section> */}
       </div>
     </>
   );
